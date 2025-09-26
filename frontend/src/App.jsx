0c60feef7b113c66ca285077e871deaa7b4158fd
Rowlec <nhoxluachop@gmail.com>
2025-09-26 13:33:18 +0700

    ADD LOGO

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
     <>
       <div>
-        a
+        <img src="/logo.png" alt="EV Battery Swap Station Management System" className="logo" />
+        <h1>EV Battery Swap Station Management System</h1>
       </div>
     </>
   )
