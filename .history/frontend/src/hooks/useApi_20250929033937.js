e47ba7edebe3731c8d53b97b8f9123d8d6cc6619
hman <lamhuynhhueman.ptht@gmail.com>
2025-10-11 17:04:22 +0700

    Delete .history

--- .history/frontend/src/hooks/useApi_20250929033937.js
+++
@@ -1,4 +0,0 @@
-// Custom hook for API calls
-export function useApi() {
-  // Hook logic will go here
-}
--- .history/frontend/src/hooks/useAsyncHandler_20251010221529.js
+++
@@ -1,9 +0,0 @@
-import { useState, useCallback } from "react";
-
-export default function useAsyncHandler(function,) {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [success, setSuccess] = useState(null);
-
-  return <div>useAsyncHandler</div>;
-}
