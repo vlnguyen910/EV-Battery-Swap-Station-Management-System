336a2a6e2adc3c2d95462663b129c8396aa55d6f
hman <lamhuynhhueman.ptht@gmail.com>
2025-10-15 06:16:26 +0700

    [FE][UPDATE]: Refactor ProtectedRoute component and restore commented routes in App component for improved access control

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,38 +10,38 @@ import NotFound from './pages/NotFound'
 import StaffDashboard from './components/dashboard/StaffDashboard'
 import StaffInventory from './components/dashboard/StaffInventory'
 import StaffInspection from './components/dashboard/StaffInspection'
-// import ProtectedRoute from './components/auth/ProtectedRoute'
+import ProtectedRoute from './components/auth/ProtectedRoute'
 
 function App() {
   return (
     <div className="App">
       {/* <Navigation /> */}
       <Routes>
-        {/* <Route
+        <Route
           path="/driver"
           element={
             <ProtectedRoute allowedRoles={["driver", "admin", "station_staff"]}>
               <Driver />
             </ProtectedRoute>
           }
-        /> */}
+        />
 
-        <Route path="/driver" element={<Driver />} />
+        {/* <Route path="/driver" element={<Driver />} /> */}
         <Route path="/" element={<GuestPage />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/login" element={<AuthContainer mode="login" />} />
         <Route path="/register" element={<AuthContainer mode="register" />} />
-        {/* <Route
+        <Route
           path="/admin"
           element={
             <ProtectedRoute allowedRoles={["admin"]}>
               <AdminPage />
             </ProtectedRoute>
           }
-        /> */}
-        <Route path="/admin" element={<AdminPage />} />
+        />
+        {/* <Route path="/admin" element={<AdminPage />} /> */}
 
-        {/* <Route
+        <Route
           path='/staff'
           element={
             <ProtectedRoute allowedRoles={["station_staff"]}>
@@ -52,17 +52,15 @@ function App() {
           <Route index element={<StaffDashboard />} />
           <Route path="inventory" element={<StaffInventory />} />
           <Route path="inspection" element={<StaffInspection />} />
-        </Route> */}
+        </Route>
 
-        <Route path="/staff" element={<StaffPage />}>
-          {/* Route con  */}
+        {/* <Route path="/staff" element={<StaffPage />}>
+           Route con  
           <Route index element={<StaffDashboard />} />
           <Route path="inventory" element={<StaffInventory />} />
           <Route path="inspection" element={<StaffInspection />} />
-        </Route>
+        </Route> */}
 
-        {/* Dashboard Routes */}
-        <Route path="/staff/inventory" element={<StaffInventory />} />
       </Routes>
     </div>
   );
