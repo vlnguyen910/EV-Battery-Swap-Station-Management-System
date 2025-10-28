a428dc62e671b28812a0272dd66a8356e56bcfb7
vlnguyen910 <volamnguyen447@gmail.com>
2025-10-28 16:40:31 +0700

    [BE][REF] Update VehiclesController to use findManyByUser method; include UsersModule in VehiclesModule imports

--- backend/src/modules/vehicles/vehicles.module.ts
+++ backend/src/modules/vehicles/vehicles.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { VehiclesService } from './vehicles.service';
 import { VehiclesController } from './vehicles.controller';
 import { DatabaseModule } from '../database/database.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, UsersModule],
   controllers: [VehiclesController],
   providers: [VehiclesService],
   exports: [VehiclesService],
