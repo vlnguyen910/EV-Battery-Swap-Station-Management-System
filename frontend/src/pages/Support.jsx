96a0c87699a50ff168eb662d190937aabfe6d69e
hman <lamhuynhhueman.ptht@gmail.com>
2025-11-07 22:22:58 +0700

    [BE][UPDATE] enhance Support component to fetch all stations on mount

--- frontend/src/pages/Support.jsx
+++ frontend/src/pages/Support.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useStation } from '../hooks/useContext';
 import SupportHeader from '../components/support/SupportHeader';
 import SupportTicketCard from '../components/support/SupportTicketCard';
 import SupportContact from '../components/support/SupportContact';
 
 export default function Support() {
-  const { stations } = useStation();
+  const { stations, fetchAllStations } = useStation();
+
+  useEffect(() => {
+    fetchAllStations();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="px-4 sm:px-6 lg:px-8 py-8">
